8ac164f663e21e58481e604f1774471435944d61
franklinjunior <franklinjunior021118@gmail.com>
2023-10-24 16:41:36 -0500

    avance

--- src/app/models/Administradores.ts
+++ src/app/models/Administradores.ts
@@ -2,38 +2,59 @@ import { sequelize } from "../config/database";
 import { DataType } from "sequelize-typescript";
 import { v4 as uuidv4 } from 'uuid';
 const Administradores = sequelize.define('Administradores',{
+    // ID DEL USUARIO
     id:{
         type:DataType.INTEGER,
         primaryKey:true,
         autoIncrement:true
     },
+    // NOMBRE DEL USUARIO
     nombre:{
         type:DataType.STRING,
         allowNull:false
     },
+    // APELLIDO DEL USUARIO
     apellido:{
         type:DataType.STRING,
         allowNull:false
     },
+    // CORREO DEL USUARIO
     correo:{
         type:DataType.STRING
     },
+    // USUARIO DEL USUARIO
     usuario:{
         type:DataType.STRING,
         allowNull:false
     },
+    // CONTRASEÑA DEL USUARIO
     contraseña:{
         type:DataType.STRING,
         allowNull:false
     },
+    // ID RROL DEL USUARIO
     id_rol:{
         type:DataType.INTEGER,
         allowNull:false
     },
+    // ESTADO DEL USUARIO BLOQUEADO O NO
     isBlocked:{
-        type:DataType.BOOLEAN,
-        
-    }
+        type:DataType.BOOLEAN,  
+    },
+    // FECHA DEL BLOQUEO DEL USUARIO
+    lock_date:{
+        type:DataType.STRING,
+    },
+    // FECHA DEL ULTIMO LOGIN DEL USUARIO
+    login_attempts:{
+        type:DataType.INTEGER,
+    },
+    // FECHA DEL ULTIMO LOGIN DEL USUARIO
+    last_login_attempt:{
+        type:DataType.STRING,
+    },
+    
+    
     
 })
 
