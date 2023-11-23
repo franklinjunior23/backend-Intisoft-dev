05534925194a68016183bebcd3f0accccb7df431
franklinjunior <franklinjunior021118@gmail.com>
2023-11-23 01:35:02 -0500

    avance ultimo

---
+++ src/app/config/database.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+const {
+  DATABASE_NAME,
+  DATABASE_PASSWORD,
+  DATABASE_USER,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_TYPE,
+} = process.env;
+export const sequelize = new Sequelize(
+  process.env.DBNAME || "inventario_roy",
+  process.env.USER || "root",
+  process.env.PASWORD || "franklin",
+  {
+    host: process.env.HOST || "",
+    port: Number(process.env.PORT) || 3306,
+    dialect:
+      "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  }
+);
+
+export const Connection = async () => {
+  try {
+    await sequelize.sync();
+  } catch (error) {
+    console.log(error);
+  }
+};
